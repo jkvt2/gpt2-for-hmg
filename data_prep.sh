538bd7e4890542c04e2f5eb620e484c5c213c3f1
jkvt2 <vince.tan.jun.kai@gmail.com>
2020-06-20 15:32:44 +0800

    included the json-creation stage using openpose

--- data_prep.sh
+++ data_prep.sh
@@ -1,6 +1,15 @@
+OPENPOSE=/path/to/openpose
+
 cd data
-mkdir raw
+mkdir raw json hmr
 ffmpeg -i videoplayback.webm -vsync 0 raw/out%d.png
 python sort_frames.py
+
+cwd=$(pwd)
+cd $OPENPOSE
+for file in $cwd/preproc/*; do
+  build/examples/openpose/openpose.bin --image_dir $cwd/preproc/"${file##*/}"/ --write_json $cwd/json/"${file##*/}"/
+done
+
 cd ../hmr
 python extract.py
